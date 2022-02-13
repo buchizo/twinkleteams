a39a943d6f2543cc7cb7d0418c6a485a43be5048
kosmos.ebi <kosmosebi@azure.moe>
2022-02-14 04:32:14 +0900

    update observe meeting speakers

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,17 +1,42 @@
 (() => {
   "use strict";
-  const target = document.getElementsByTagName('calling-screen');
-  const observer = new MutationObserver(function (mutations) {
-    console.table(mutations);
+  console.log("[twinkle2teams] start extension...");
+
+  const body_observer = new MutationObserver(function (mutations) {
+    for (const mutation of mutations) {
+      if (mutation.type !== "attributes") continue;
+      if (!mutation?.target?.classList) continue;
+      if (mutation.target.classList.contains("ts-calling-participant-stream")) {
+        const newval = mutation.target.getAttribute(mutation.attributeName);
+        for (const c1 of mutation.target.children) {
+          if (c1.classList.contains("person-name-layer")) {
+            for (const c2 of c1.children) {
+              if (c2.classList.contains("person-name")) {
+                for (const c3 of c2.children) {
+                  if (c3.className === "lpc-hoverTarget") {
+                    const status = {
+                      isSpeaking: newval.includes("speaking"),
+                      isMuted: newval.includes("is-muted"),
+                      name: c3.innerText
+                    };
+                    console.log(status);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   });
 
-  observer.observe(target, {
+  body_observer.observe(document.body, {
     attributes: true,
-    attributeOldValue: false,
+    attributeOldValue: true,
     characterData: false,
     characterDataOldValue: false,
     childList: true,
     subtree: true,
-    attributeFilter: ['class']
+    attributeFilter: ["class"]
   });
 })();
