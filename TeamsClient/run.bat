16e08bcef0197cc9300c4590ebc918208b2d7f92
kosmos.ebi <kosmosebi@azure.moe>
2024-08-30 04:03:49 +0900

    update packages

--- TeamsClient/run.bat
+++ TeamsClient/run.bat
@@ -1,1 +1,1 @@
-npx webpack-dev-server --entry ./client.js --output bundle.js --debug --devtool inline-source-map
+npx webpack-dev-server 
--- TeamsClient/webpack.config.js
+++ TeamsClient/webpack.config.js
@@ -1,12 +1,26 @@
+const path = require("path");
+
 module.exports = {
-	entry: './js/client.js',
-    devServer: {
-        port: 8080,
-        proxy: {
-            '/statusHub': {
-                target: 'http://localhost:5132',
-                ws: true
-            }
-        }
-    }
+  entry: {
+    index: "./client.js",
+  },
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, ".dist"),
+  },
+  devServer: {
+    port: 8080,
+    static: {
+        serveIndex: true,
+        directory: "."
+    },
+    proxy: [
+      {
+        context: ["/statusHub"],
+        target: "http://localhost:5132",
+        ws: true,
+      },
+    ],
+  },
 };
